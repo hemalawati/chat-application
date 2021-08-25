11209547ded7a0a1b41b8aa90aa978c913510edf
hemalawati <root@Hemas-MacBook-Pro.local>
2021-08-25 16:33:30 -0400

    installed dependencies and created css and main page

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import { ChatEngine } from 'react-chat-engine';
+import './app.css';
+
+const App = () => {
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID="11ab9f63-ebb1-47f9-a67d-b9153ed1a3c9"
+      userName="hemalawati"
+      userSecret="chat2021<%EnGine"
+    />
+  );
+};
+
+export default App;
