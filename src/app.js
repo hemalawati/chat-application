25ed09e6096b4bcf39a890a7b6088e5cd51120cc
hemalawati <root@Hemas-MacBook-Pro.local>
2021-08-25 23:46:46 -0400

    Login Form

--- src/app.js
+++ src/app.js
@@ -1,15 +1,18 @@
 import { ChatEngine } from "react-chat-engine";
 import ChatFeed from "./components/ChatFeed";
+import LoginForm from "./components/LoginForm";
 import "./app.css";
 
 const App = () => {
+  if (!localStorage.getItem("username")) return <LoginForm />;
+
   return (
     <ChatEngine
       height="100vh"
       projectID="11ab9f63-ebb1-47f9-a67d-b9153ed1a3c9"
-      userName="hemalawati"
-      userSecret="chat2021<%EnGine"
-      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps}/>}
+      userName={localStorage.getItem("username")}
+      userSecret={localStorage.getItem("password")}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
     />
   );
 };
